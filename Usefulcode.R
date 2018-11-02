124b6b74d5660ccd11167612402693756259e460
SebVen <venckens@tcd.ie>
2018-11-02 19:01:12 +0000

    Update Usefulcode.R

---
+++ Usefulcode.R
@@ -0,0 +1,13 @@
+#These are some short functions that are useful for clinical trial analysis and producing clinical study reports
+
+#Rename columns to tidy names
+require(tidyverse)
+tibl %>%
+  rename_all(funs(make.names(.))) %>%
+  rename_all(funs(str_replace(.,"\\.{2,}","\\."))) %>%
+  rename_all(funs(tolower(.)))
+
+#Remove all rows if NA excluding certain columns
+require(tidyverse)
+tibl %>%
+  dplyr::filter_at(vars(-COLUMN1, -COLUMN2), any_vars(!is.na(.)))
