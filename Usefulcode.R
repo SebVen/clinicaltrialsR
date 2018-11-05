939b48ec9c37b99cdcbce74aaa76cb23dd740be8
SebVen <venckens@tcd.ie>
2018-11-05 14:46:50 +0000

    Update Usefulcode.R

--- Usefulcode.R
+++ Usefulcode.R
@@ -2,18 +2,18 @@
 
 #Rename variable names to tidy names
 require(tidyverse)
-tibl %>%
+TIBL %>%
   rename_all(funs(make.names(.))) %>%
   rename_all(funs(str_replace(.,"\\.{2,}","\\."))) %>%
   rename_all(funs(tolower(.)))
 
 #Remove all rows if NA excluding certain columns
 require(tidyverse)
-tibl %>%
+TIBL %>%
   dplyr::filter_at(vars(-VARIABLE1, -VARIABLE2), any_vars(!is.na(.)))
 
 #Add randomisation key to cases
-tibl %>%
+TIBL %>%
   left_join(TIBLWITH_RANDOMISATION_KEYS) %>%
   dplyr::select(CASE_ID_VARIABLE, RANDOMISATION_KEY_VARIABLE, everything())
 
@@ -36,7 +36,7 @@ output:
 </style>
   
 #Remove rows with NA is specific variable
-tibl %>% 
+TIBL %>% 
   dplyr::filter(!is.na(VARIABLE))
 
 #
